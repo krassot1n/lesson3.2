2d23d3a72a4bb280009d56c12aaab5f3cc485139
krassot1n <krassotin68@gmail.com>
2023-07-22 14:13:29 +0800

    initial commit

---
+++ src/main/java/ru/hogwarts/school/Schoolv1Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schoolv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schoolv1Application.class, args);
+    }
+
+}
